35131201beaa191572b1295eb999754b3b907103
Marvin Sosa <marvin.sosa_avantica@scotiabank.com.pe>
2022-04-08 06:03:20 -0500

    add

--- vars/runPython.groovy
+++ vars/runPython.groovy
@@ -1,8 +1,8 @@
-def call( String config ) {
+def call( String host, String dias, String token ) {
   final pythonContent = libraryResource('com/scotiabank/sharedlib/test.py')
   writeFile(file: 'test.py', text: pythonContent)
   sh """
      chmod +x test.py
-     echo "./test.py ${config}"
+     echo "./test.py ${host} ${dias} ${token}"
      """
 }
