58f951ae35569c7559b649257a087f112549f90e
Marvin Sosa <marvin.sosa_avantica@scotiabank.com.pe>
2022-04-08 05:38:28 -0500

    add

--- vars/runPython.groovy
+++ vars/runPython.groovy
@@ -1,4 +1,4 @@
-def maintenance_sonar( urldiastoken ) {
+def call( urldiastoken ) {
   final pythonContent = libraryResource('com/scotiabank/sharedlib/test.py')
   writeFile(file: 'test.py', text: pythonContent)
   sh('chmod +x test.py && echo "./test.py ${urldiastoken} ${urldiastoken} ${urldiastoken}" ')
